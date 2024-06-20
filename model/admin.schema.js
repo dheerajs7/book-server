ad6d58e6663583d1c30c32f287423b90e2124eda
dheerajs7 <dheeraj.salvi293@gmail.com>
2024-06-20 13:57:29 +0530

    modal changes

--- model/admin.schema.js
+++ model/admin.schema.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
- const adminSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number
-})
+const adminSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
 
-export const Admin = new mongoose.model('Admin',adminSchema)
-
+export const Admin = new mongoose.model("Admin", adminSchema);
